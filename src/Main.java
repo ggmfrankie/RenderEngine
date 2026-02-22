a94d8585972654a4615e5a194dfdcfe4cf90eebe
Stefan <ertl.stefan2012@gmail.com>
2026-02-22 17:49:27 +0100

    Nah

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,6 @@ public class Main {
     public static void main(String[] args) {
         gameEngine = new GameEngine("Game", 1000, 1000, new DummyGame());
         gameEngine.start();
-        double delta_alpha = 0.1;
-        var KotlinTest = new KotlinTest();
-
-        String s = "19,8\n" +
-                "18,3\n" +
-                "17,3\n" +
-                "15,7\n" +
-                "13,5\n" +
-                "11,5\n" +
-                "9,7\n" +
-                "8,2\n" +
-                "6,8\n" +
-                "5,7\n" +
-                "4,6\n" +
-                "3,6\n" +
-                "2,8\n" +
-                "2\n" +
-                "1,3\n" +
-                "0,7\n" +
-                "0,1\n" +
-                "0,6\n";
-
-        refactorString(s);
-
-        double[] werte = new double[]{800.042- 798.243, 801.847- 800.042, 803.612- 801.847, 805.460- 803.612, 807.283- 805.460};
-
-        //average(werte);
-
     }
 
 
